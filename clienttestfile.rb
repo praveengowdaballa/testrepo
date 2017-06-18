b861f3f53f66731f320debc4de5cd9c4c2615352
praveen gowda <praveen gowda>
2017-06-18 09:47:06 +0530

    Update instance type

--- clienttestfile.rb
+++ clienttestfile.rb
@@ -1,4 +1,4 @@
-1. Create t2.small
+1. Run t2.medium instance
 2. Wget https://packages.chef.io/files/stable/chef-server/12.15.7/el/7/chef-server-core-12.15.7-1.el7.x86_64.rpm
 3. Run rpm -ivh chef-server-core-12.15.7-1.el7.x86_64.rpm
 4. chef-server-ctl reconfigure
